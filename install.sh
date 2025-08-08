6621e562dd969fd4ea2a7a557bfc3799e32e5088
Delian Delchev <delian.delchev@gmail.com>
2025-08-08 18:30:42 +0200

    next
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-mkdir -p ~/.code-server/bin
+set -e
 
-tar -zxvf <(curl -Lo - https://code.visualstudio.com/sha/download\?build\=stable\&os\=cli-alpine-x64) -C ~/.code-server/bin
-if [ $? -ne 0 ]; then
-    echo "Failed to download or extract the Visual Studio Code tarball."
-    exit 1
-fi
+mkdir -p ~/.code-server/bin
+tar -zxvf <(curl -Lo - https://code.visualstudio.com/sha/download\?build\=stable\&os\=cli-alpine-x64) -C ~/.code-server/bin || { echo "Failed to download or extract the Visual Studio Code tarball."; exit 1; }
 
